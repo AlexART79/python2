e54a0c5d70cde2306b4a1cf7ae1f2a656c54713a
Alekandr Artemov <aartemov@lohika.com>
2018-09-07 16:17:16 +0300

    phase 3

---
+++ phase3/src/tests/fixtures/issue_info.py
@@ -0,0 +1,42 @@
+import pytest
+import json
+
+from src.jira import Jira
+
+# class to store issue data for tests
+class IssueInfo:
+    def __init__(self, project=None, summary=None, description=None, type=None, expected=None):
+        self.project = {"key": project} if project is not None else False
+        self.summary = summary
+        self.description = description
+        self.type = {"name": type} if type is not None else False
+
+        self.expected = expected
+
+    def get_json(self):
+        dict_asd = {"fields": {}}
+        if self.project:
+            dict_asd["fields"]['project'] = self.project
+
+        if self.summary:
+            dict_asd["fields"]['summary'] = self.summary
+        if self.description:
+            dict_asd["fields"]['description'] = self.description
+        if self.type:
+            dict_asd["fields"]['issuetype'] = self.type
+
+        return dict_asd
+
+
+# fixture to create issue before tests (for delete, update tests)
+@pytest.fixture
+def prep_issue():
+    i = IssueInfo("AQAPYTHON", "AA004 - issue_to_be_deleted", "this is a test issue for delete test ", "Bug")
+
+    jira = Jira()
+    jira.authenticate("Alexander_Artemov", "Alexander_Artemov")
+
+    r = jira.create_issue(i)
+    data = json.loads(r.content)
+
+    return data["key"]
