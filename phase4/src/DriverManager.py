07ed418c029ba2872ecf0a16970e165f1b4839cc
Alekandr Artemov <aartemov@lohika.com>
2018-09-17 13:25:07 +0300

    driver manager

--- phase4/src/DriverManager.py
+++ phase4/src/DriverManager.py
@@ -5,6 +5,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 class DriverManager:
     @staticmethod
     def chrome_driver():
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--kiosk")
-        return webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        #chrome_options = webdriver.ChromeOptions()
+        #chrome_options.add_argument("--kiosk")
+        return webdriver.Chrome(ChromeDriverManager().install()) #, options=chrome_options)
