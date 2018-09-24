36b0da772409ea3e351e9e6d17bb4bca1243c8bc
Alekandr Artemov <aartemov@lohika.com>
2018-09-24 13:37:54 +0300

    wip

--- phase7_behave/src/features/login.feature
+++ phase7_behave/src/features/login.feature
@@ -1,6 +1,21 @@
 Feature: Login
 
-Scenario: Login with correct credentials
+Background:
     Given open login page in browser
-     When user logging in using 'Alexander_Artemov', 'Alexander_Artemov'
+
+Scenario: Login with correct credentials
+     When user logging in using "Alexander_Artemov", "Alexander_Artemov"
      Then user should be successfully logged in
+
+
+Scenario Outline: Login Incorrect
+     When user logging in using "<username>", "<password>"
+     Then user should get a login error message
+
+  Examples:
+    | username          | password |
+    | alex_art          | test     |
+    | alex_art          | -        |
+    | -                 | test     |
+    | Alexander_Artemov | -        |
+
--- phase7_behave/src/features/steps/login.py
+++ phase7_behave/src/features/steps/login.py
@@ -1,4 +1,5 @@
 import allure
+
 from allure_commons.types import AttachmentType
 from behave import *
 
@@ -6,11 +7,21 @@ from src.DriverManager import DriverManager
 from src.pages.pages import GeneralPage, DashboardPage, IssuesSearchPage, LoginPage
 
 
+@fixture
+def selenium_browser_chrome(context):
+    context.browser = DriverManager.chrome_driver()
+    yield context.browser
+    context.browser.quit()
+
+
+def before_scenario(context, scenario):
+    use_fixture(selenium_browser_chrome, context)
+
+
 @allure.step
 @allure.tag('UI')
 @given("open login page in browser")
 def open_login_page_in_browser(context):
-    context.driver = DriverManager.chrome_driver()
     context.login_page = LoginPage(context.driver)
     context.login_page.go("http://jira.hillel.it:8080/")
     allure.attach(context.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
@@ -18,7 +29,7 @@ def open_login_page_in_browser(context):
 
 @allure.step
 @allure.tag('UI')
-@when("user logging in using '{username}', '{password}'")
+@when('user logging in using "{username}", "{password}"')
 def user_logging_in_using(context, username, password):
     context.login_page.login(username, password)
     allure.attach(context.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
@@ -30,3 +41,11 @@ def user_logging_in_using(context, username, password):
 def user_should_be_successfully_logged_in(context):
     allure.attach(context.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
     assert context.login_page.is_logged_in
+
+
+@allure.step
+@allure.tag('UI')
+@then("user should get a login error message")
+def user_should_get_a_login_error_message(context):
+    allure.attach(context.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+    assert "your username and password are incorrect" in context.login_page.login_error_message
