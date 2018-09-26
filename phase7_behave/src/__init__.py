526b760d9fec521784c00d6d805fad810319f7fe
Alekandr Artemov <aartemov@lohika.com>
2018-09-26 16:14:58 +0300

    imports!

--- phase7_behave/src/__init__.py
+++ phase7_behave/src/__init__.py
@@ -3,6 +3,8 @@ import os
 
 home_dir = os.path.expanduser("~")
 sys.path.append(os.path.join(home_dir, 'phase7_behave/src'))
+sys.path.append("/home/circleci/repo/phase7_behave/src")
+sys.path.append("/home/circleci/repo/phase7_behave")
 
 # making absolute references shorter...
 from .features.steps.common import transform_parameters
